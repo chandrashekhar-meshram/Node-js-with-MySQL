558c5cd80fbc52b3c3fa31af7f0914a20288ea1b
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-04 23:22:36 +0530

    post api

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.get("/", (req, resp) => {
   });
 });
 
+app.post('/', (req, resp) => {
+  const data = {id: "6", title: "shirt 5", price: "3000", description: "shirt", category: "clothing", image: "test image"};
+
+  con.query('INSERT INTO products SET ?', data, (err, result, fields) => {
+    if(err) throw err;
+    resp.send(result);
+  })
+});
+
 console.log("hi");
 
 app.listen("5000");
